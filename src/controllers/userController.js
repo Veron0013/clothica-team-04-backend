5101fc315e58799e2891471471ec0b011ee2c83c
Alyona <oranchuka@gmail.com>
2025-11-08 12:18:49 +0200

    user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,14 +33,16 @@ export const updateUserProfile = async (req, res) => {
 	res.status(200).json(user);
 }
 
-export const getUser = async (req, res) => {
 
-	const userId = req.user._id;
-	const user = await User.findOne({ _id: userId })
+export const getCurrentUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw createHttpError(404, "User not found");
+    }
 
-	if (!user) {
-		throw createHttpError(404, "user not found")
-	}
-
-	res.status(200).json(user);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
